01ffeb912f3d44c8111b908ebcdca096b23fc192
AlexaEssence <132620670+AlexaEssence@users.noreply.github.com>
2023-06-26 17:44:59 -0400

    Add files via upload

---
+++ Beenatis fav bonsai rolls/events.js
@@ -0,0 +1,7 @@
+function open_navbar() {
+    document.getElementById("navbar").style.display = "flex";
+}
+
+function close_navbar() {
+    document.getElementById("navbar").style.display = "none";
+}
